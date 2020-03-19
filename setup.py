4df2efef40ae85758d1beb7897289000df4cb022
Ergin Soysal <esoysal@gmail.com>
2020-03-19 16:53:31 -0500

    better package versioning

--- setup.py
+++ setup.py
@@ -1,26 +1,43 @@
+import os
+import re
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+def read(*parts):
+    with open(os.path.join(PWD, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^\s*__version__\s*=\s*['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+PWD = os.path.abspath(os.path.dirname(__file__))
+PACKAGE = 'appfw'
+VERSION = find_version(PACKAGE, '__init__.py')
 
 
 setuptools.setup(
-    name='appfw',
-    version='0.6',
+    name=PACKAGE,
+    version=find_version(PACKAGE, '__init__.py'),
     author="Ergin Soysal",
     author_email="esoysal@gmail.com",
     description="Application framework",
-    long_description=long_description,
+    long_description= read('README.md'),
     long_description_content_type="text/markdown",
     url="https://github.com/soysal/appfw",
     packages=setuptools.find_packages(),
-    package_data={'appfw': ['tpl/*.tpl']},
+    package_data={PACKAGE: ['tpl/*.tpl']},
     entry_points={
         'console_scripts': ['app-init=appfw.app_init:run'],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)",
          "Operating System :: OS Independent",
     ],
 )
